020eb19d854c0666bd17c5e297d4af028d0b4d7c
kartiksharma1666 <kartiksharma1666@gmail.com>
2024-12-05 22:22:02 +0530

    CoinBase 1 Dec 2024

--- Desiqna/CoinBase_1_dec_2024.cpp
+++ Desiqna/CoinBase_1_dec_2024.cpp
@@ -4,24 +4,30 @@ using namespace std;
 
 int countSubsetsSizeKDivisibleByM(int N, int M, int K) {
     vector<vector<vector<int>>> dp(N + 1, vector<vector<int>>(M, vector<int>(K + 1, 0)));
-    dp[0][0][0] = 1;  // 1 way to make sum 0 with 0 elements and size 0
+    dp[0][0][0] = 1;  // 1 way to make sum 0 with 0 elements
 
     for (int i = 1; i <= N; i++) {
+        int v1 = (i - 1) % M;  // Current value modulo M
         for (int j = 0; j < M; j++) {
             for (int k = 0; k <= K; k++) {
-                dp[i][j][k] = dp[i - 1][j][k];  // Ignore current element
+                // Ignore the current element
+                dp[i][j][k] = dp[i - 1][j][k];
+
+                // Include the current element if k > 0
                 if (k > 0) {
-                    int remainder = (j - (i - 1) % M + M) % M;
-                    dp[i][j][k] += dp[i - 1][remainder][k - 1];  // Include current element
+                    int l = (j >= v1) ? (j - v1) : (M - v1 + j);
+                    dp[i][j][k] += dp[i - 1][l][k - 1];
                 }
             }
         }
     }
-    return dp[N][0][K];  // Return subsets with sum divisible by M and size K
+
+    return dp[N][0][K];  // Ways to get sum divisible by M with exactly K elements
 }
 
 int main() {
     int N = 5, M = 3, K = 2;
-    cout << "Ways to pick subset of size " << K << " with sum divisible by " << M << ": " << countSubsetsSizeKDivisibleByM(N, M, K) << endl;
+    cout << "Ways to get sum divisible by " << M << " with subset size " << K 
+         << ": " << countSubsetsSizeKDivisibleByM(N, M, K) << endl;
     return 0;
 }
